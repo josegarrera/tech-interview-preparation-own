69fdb4770a53a0fb8585b772ecbb3b3518d46111
josegarrera <josegarrera@josegarrera-IdeaPad-3-15IIL05>
2021-05-12 11:03:42 -0300

    FindNeedle ready

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,6 +1,27 @@
-function FindNeedle(haystack, needle) {
-  // Your code here:
+// function FindNeedle(haystack, needle) {
+// 	let iter = 0;
+// 	for (let i = 0; i < haystack.length; i++) {
+// 		if (haystack[i] === needle[iter]) {
+// 			if (iter === needle.length - 1) {
+// 				return i - needle.length;
+// 			}
+// 			iter++;
+// 		} else {
+// 			iter = 0;
+// 		}
+// 	}
+// 	return -1;
+// }
 
+function FindNeedle(haystack, needle) {
+	for (let i = 0; i < haystack.length; i++) {
+		if (haystack.slice(i, i + needle.length) === needle) {
+			return i;
+		}
+	}
+	return -1;
 }
+
+console.log(FindNeedle('rereredux', 'reredux'));
 
 module.exports = FindNeedle;
