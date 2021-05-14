1be053925c99abe8241098e51a57c3f345f068eb
josegarrera <josegarrera@josegarrera-IdeaPad-3-15IIL05>
2021-05-14 09:58:20 -0300

    Exercise 06 ready

--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,25 @@
 function HasBalancedBrackets(string) {
-  // Your code here:
+	const dictionary = {
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	};
 
+	let stack = [];
+	for (let i = 0; i < string.length; i++) {
+		if (dictionary[string[i]]) {
+			stack.push(string[i]);
+		} else {
+			let last = stack.pop();
+			if (dictionary[last] !== string[i]) {
+				return false;
+			}
+		}
+	}
+	return !stack.length;
 }
+
+// O(n) temporal dónde n es el length del string
+// O(n) espacial dónde n es la cantidad máxima de caracteres abiertos sin cerrar
 
 module.exports = HasBalancedBrackets;
